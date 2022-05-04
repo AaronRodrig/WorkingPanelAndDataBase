a141b6b17c12fc10e178979f72b30e913483074b
aaron <aaronrod77@hotmail.com>
2022-05-04 23:05:52 +0100

    feat:added InsertTable class to insert table values

---
+++ src/groupdatabase/InsertTable.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package groupdatabase;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InsertTable {
+        public static void insertEmp() {
+        Connection con = ConnectDB.getConnection();
+        Statement stmt = null;
+        String sqlString = ("INSERT INTO Employee  \n"
+              +  "VALUES (1066789101, 'Simpson', 'Worker', 20010),\n"
+              + "(1066789102, 'Walker', 'Worker', 12345)");
+        try {
+            con.setAutoCommit(false);
+            stmt = con.createStatement();
+            stmt.executeUpdate(sqlString);
+            stmt.close();
+            con.commit();
+        } catch (SQLException ex) {
+            System.err.println("SQLException: " + ex.getMessage());
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    System.err.println("SQLException: " + e.getMessage());
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.err.println("SQLException: " + e.getMessage());
+                }
+            }
+        }
+    }
+}
